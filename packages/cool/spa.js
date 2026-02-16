043b9a5ae8e191b1c59c750fcc3f6bc6ec8d8130
lycHub <1120805364@qq.com>
2026-02-16 15:08:47 +0800

    feat(spa\): spa server

--- packages/cool/spa.js
+++ packages/cool/spa.js
@@ -1,14 +1,25 @@
 import { join } from 'node:path';
 
 import express from 'express';
-import history from 'connect-history-api-fallback';
 
 import { getDirname } from './express/utils/dirname.js';
 
 const app = express();
 const __dirname = getDirname(import.meta.url);
-app.use(history());
-app.use(express.static(join(__dirname, 'dist')));
+
+const isStaticResource = (path) => {
+  return /\.(js|css|png|jpg|jpeg|gif|svg|ico|webp|woff|woff2|ttf|eot|json|map)$/.test(path);
+};
+
+app.use('/spa', express.static(join(__dirname, 'dist')));
+
+app.use((req, res, next) => {
+  if (req.path.startsWith('/spa') && !isStaticResource(req.path)) {
+    res.sendFile(join(__dirname, 'dist', 'index.html'));
+  } else {
+    next();
+  }
+});
 
 app.use((err, req, res, next) => {
   res.status(500).send('Something broken!!!');
@@ -16,5 +27,5 @@ app.use((err, req, res, next) => {
 
 const PORT = 3335;
 app.listen(PORT, '::', () => {
-  console.log('Listen at http://localhost:' + PORT);
+  console.log('Listen at http://localhost:' + PORT + '/spa');
 });
