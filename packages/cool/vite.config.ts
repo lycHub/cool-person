7d16b0b463c24b5105baafa79b0b371ef2e4ad10
lycHub <1120805364@qq.com>
2026-02-16 13:22:05 +0800

    feat(public): public path

--- packages/cool/vite.config.ts
+++ packages/cool/vite.config.ts
@@ -4,12 +4,12 @@ import vue from '@vitejs/plugin-vue';
 import { defineConfig, loadEnv } from 'vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
-export default defineConfig(({ mode, isSsrBuild }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, join(__dirname, 'envs'), 'VITE_');
   return {
     plugins: [vue(), vueDevTools()],
     envDir: 'envs',
-    base: isSsrBuild ? env.VITE_PUBLIC_PATH_SSR : env.VITE_PUBLIC_PATH,
+    base: env.VITE_PUBLIC_PATH,
     server: {
       host: '0.0.0.0',
     },
